3d07930181b8871d79a54410fb2c3c6a921977e0
Arumugam Maharaja <raja1998civil@gmail.com>
2025-09-10 11:18:49 +0530

    Removed local volume mounts

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -13,7 +13,7 @@ from typing import List, Dict, Any
 import pandas as pd
 from tqdm import tqdm
 from neo4j_driver import Neo4jHandler
-
+MISSING_VALUE = "" #MISSING_VALUE = "missing", Initially I removed missing values, but later I decided to keep them for relationship creation.
 
 def get_env_variable(var_name: str, default: str) -> str:
     """Get environment variable or default."""
@@ -89,7 +89,7 @@ def main() -> None:
         'prlOriginalArea', 'prlCurrentArea', 'prlPhaseCurrent', 'prlFactPageUrl'
     ]
     license_df = fill_and_strip(license_df)
-    licenses = remove_all_occurrences(list(license_df['prlName'].unique()), "missing")
+    licenses = remove_all_occurrences(list(license_df['prlName'].unique()), MISSING_VALUE)
     licenses_with_properties = get_properties(license_df, licenses, 'prlName', license_properties_columns)
 
     # Field
@@ -109,9 +109,9 @@ def main() -> None:
     ]
     field_df = field_df[field_useful_columns].rename(columns=field_rename_mapping)
     field_df = fill_and_strip(field_df)
-    fields = remove_all_occurrences(list(field_df['fldName'].unique()), "missing")
+    fields = remove_all_occurrences(list(field_df['fldName'].unique()), MISSING_VALUE)
     fields_with_properties = get_properties(field_df, fields, 'fldName', field_properties_columns)
-    supply_bases = remove_all_occurrences(list(field_df['fldMainSupplyBase'].unique()), "missing")
+    supply_bases = remove_all_occurrences(list(field_df['fldMainSupplyBase'].unique()), MISSING_VALUE)
 
     # Well
     well_path = os.path.join(CSV_DIR, "wellbore_all_long.csv")
@@ -137,16 +137,16 @@ def main() -> None:
     }
     well_df = wlb_df[useful_columns].rename(columns=well_rename_mapping)
     well_df = fill_and_strip(well_df)
-    areas = remove_all_occurrences(list(well_df['wlbMainArea'].unique()), "missing")
-    field_names = remove_all_occurrences(list(well_df['wlbField'].unique()), "missing")
-    discoveries = remove_all_occurrences(list(well_df['wlbDiscovery'].unique()), "missing")
-    wells = remove_all_occurrences(list(well_df['wlbWell'].unique()), "missing")
+    areas = remove_all_occurrences(list(well_df['wlbMainArea'].unique()), MISSING_VALUE)
+    field_names = remove_all_occurrences(list(well_df['wlbField'].unique()), MISSING_VALUE)
+    discoveries = remove_all_occurrences(list(well_df['wlbDiscovery'].unique()), MISSING_VALUE)
+    wells = remove_all_occurrences(list(well_df['wlbWell'].unique()), MISSING_VALUE)
     wells_with_properties = get_well_properties(well_df, wells, properties_columns)
-    wellbores = remove_all_occurrences(list(well_df['wlbWellboreName'].unique()), "missing")
+    wellbores = remove_all_occurrences(list(well_df['wlbWellboreName'].unique()), MISSING_VALUE)
     wellbore_with_properties = get_wellbore_properties(well_df, wellbores, properties_columns)
-    drilling_facilities = remove_all_occurrences(list(well_df['wlbDrillingFacility'].unique()), "missing")
-    facilities = remove_all_occurrences(list(well_df['wlbFacilityTypeDrilling'].unique()), "missing")
-    operators = remove_all_occurrences(list(well_df['wlbDrillingOperator'].unique()), "missing")
+    drilling_facilities = remove_all_occurrences(list(well_df['wlbDrillingFacility'].unique()), MISSING_VALUE)
+    facilities = remove_all_occurrences(list(well_df['wlbFacilityTypeDrilling'].unique()), MISSING_VALUE)
+    operators = remove_all_occurrences(list(well_df['wlbDrillingOperator'].unique()), MISSING_VALUE)
 
     print("Data loaded and cleaned successfully.")
 
@@ -167,22 +167,15 @@ def main() -> None:
         missed_indexes = []
         for index, row in tqdm(well_df.iterrows(), total=well_df.shape[0], desc="Creating Well Relationships"):
             try:
-                if row["wlbMainArea"] != 'missing' and row['wlbProductionLicence'] != 'missing':
-                    handler.create_edge("Area", "name", row["wlbMainArea"], "License", "name", row['wlbProductionLicence'], "HAS_LICENSE")
-                if row["wlbProductionLicence"] != 'missing' and row["wlbField"] != "missing":
-                    handler.create_edge("License", "name", row["wlbProductionLicence"], "Field", "name", row['wlbField'], "HAS_FIELD")
-                if row["wlbField"] != 'missing' and row["wlbDiscovery"] != "missing":
-                    handler.create_edge("Field", "name", row["wlbField"], "Discovery", "name", row['wlbDiscovery'], "HAS_DISCOVERY")
-                if row["wlbDiscovery"] != "missing":
-                    handler.create_edge("Field", "name", row["wlbField"], "Discovery", "name", row['wlbDiscovery'], "HAS_DISCOVERY")
-                if row['wlbWell'] != row['wlbWellboreName']:
-                    handler.create_edge("Well", "name", row["wlbWell"], "Wellbore", "name", row['wlbWellboreName'], "HAS_WELLBORE")
-                if row['wlbDrillingFacility'] != "missing":
-                    handler.create_edge("DrillingFacility", "name", row["wlbDrillingFacility"], "Wellbore", "name", row['wlbWellboreName'], "HAS_WELL")
-                if row['wlbFacilityTypeDrilling'] != "missing":
-                    handler.create_edge("Wellbore", "name", row["wlbWellboreName"], "FacilityType", "name", row['wlbFacilityTypeDrilling'], "HAS_FACILITY_TYPE_OF")
-                if row['wlbDrillingOperator'] != "missing":
-                    handler.create_edge("Wellbore", "name", row["wlbWellboreName"], "Operator", "name", row['wlbDrillingOperator'], "WAS_OPERATED_BY")
+                
+                handler.create_edge("Area", "name", row["wlbMainArea"], "License", "name", row['wlbProductionLicence'], "HAS_LICENSE")
+                handler.create_edge("License", "name", row["wlbProductionLicence"], "Field", "name", row['wlbField'], "HAS_FIELD")
+                handler.create_edge("Field", "name", row["wlbField"], "Discovery", "name", row['wlbDiscovery'], "HAS_DISCOVERY")
+                handler.create_edge("Discovery", "name", row["wlbField"], "Well", "name", row['wlbWell'], "HAS_WELL")
+                handler.create_edge("Well", "name", row["wlbWell"], "Wellbore", "name", row['wlbWellboreName'], "HAS_WELLBORE")
+                handler.create_edge("Wellbore", "name", row["wlbWellboreName"], "DrillingFacility", "name", row['wlbDrillingFacility'], "HAS_DRILLING_FACILITY")
+                handler.create_edge("Wellbore", "name", row["wlbWellboreName"], "FacilityType", "name", row['wlbFacilityTypeDrilling'], "HAS_FACILITY_TYPE_OF")
+                handler.create_edge("Wellbore", "name", row["wlbWellboreName"], "Operator", "name", row['wlbDrillingOperator'], "WAS_OPERATED_BY")
             except Exception as err:
                 traceback.print_exc()
                 print(f"Error occurred in index {index}: {err}")
@@ -191,10 +184,8 @@ def main() -> None:
         # Relationships for fields
         for index, row in tqdm(field_df.iterrows(), total=field_df.shape[0], desc="Creating Field Relationships"):
             try:
-                if row["cmpLongName"] != 'missing':
-                    handler.create_edge("Operator", "name", row["cmpLongName"], "Field", "name", row['fldName'], "MANAGES")
-                if row["fldMainSupplyBase"] != "missing":
-                    handler.create_edge("Field", "name", row["fldName"], "Base", "name", row['fldMainSupplyBase'], "HAS_SUPPLYBASE_OF")
+                handler.create_edge("Operator", "name", row["cmpLongName"], "Field", "name", row['fldName'], "MANAGES")
+                handler.create_edge("Field", "name", row["fldName"], "Base", "name", row['fldMainSupplyBase'], "HAS_SUPPLYBASE_OF")
             except Exception as err:
                 traceback.print_exc()
                 print(f"Error occurred in field index {index}: {err}")
